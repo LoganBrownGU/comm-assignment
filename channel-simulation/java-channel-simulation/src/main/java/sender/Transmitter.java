e06cf3a610af110518fa1a96a35573383b88a903
LoganBrownGU <2641407b@student.gla.ac.uk>
2023-11-24 17:01:29 +0000

    added transmitter

---
+++ channel-simulation/java-channel-simulation/src/main/java/sender/Transmitter.java
@@ -0,0 +1,31 @@
+package sender;
+
+public class Transmitter {
+
+    private Modulator modulator;
+    private Filter filter;
+
+    double send(double t) {
+        double f = modulator.output(t);
+        if (filter != null) f = filter.filter(f);
+
+        return f;
+    }
+
+    public Transmitter(Modulator modulator) {
+        this.modulator = modulator;
+        filter = null;
+    }
+
+    public Transmitter(Filter filter) {
+        this.filter = filter;
+    }
+
+    public void setModulator(Modulator modulator) {
+        this.modulator = modulator;
+    }
+
+    public void setFilter(Filter filter) {
+        this.filter = filter;
+    }
+}
