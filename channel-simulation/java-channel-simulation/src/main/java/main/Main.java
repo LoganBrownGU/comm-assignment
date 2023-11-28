bde13416dcf511c6815ea299b90d7e1defafb73d
LoganBrownGU <2641407b@student.gla.ac.uk>
2023-11-28 15:07:42 +0000

    improved simulation

--- channel-simulation/java-channel-simulation/src/main/java/main/Main.java
+++ channel-simulation/java-channel-simulation/src/main/java/main/Main.java
@@ -3,8 +3,6 @@ package main;
 import channel.Channel;
 import org.jfree.data.xy.XYDataItem;
 import receiver.ASKDemodulator;
-import receiver.DataOut;
-import receiver.Demodulator;
 import receiver.Receiver;
 import simulator.Simulator;
 import transmitter.ASKModulator;
@@ -15,6 +13,19 @@ import transmitter.Transmitter;
 import java.util.ArrayList;
 
 public class Main {
+
+    public static void byteListToString(ArrayList<Byte> bytes) {
+        for (byte b : bytes) {
+            String str = Integer.toBinaryString(b);
+            if (str.length() > 8) {
+                System.out.print(str.substring(24) + " ");
+            } else {
+                for (int i = 8; i > str.length(); i--) System.out.print("0");
+                System.out.print(str + " ");
+            }
+        }
+        System.out.println();
+    }
 
     /*
     * EXPLANATION OF THE FOLLOWING BIT OPERATIONS:
@@ -76,15 +87,15 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        double noise = 24;
-        double depth = .8, amplitude = 1, carrierF = 5, modulationF = 1;
+        double noise = 100;
+        double depth = .8, amplitude = 1, carrierF = 20, modulationF = 5;
         ASKModulator modulator = new ASKModulator(depth, amplitude, carrierF, modulationF, new RandomStream());
         Channel channel = new Channel(null, modulator.getRMS(), noise);
         Receiver receiver = new Receiver(new ASKDemodulator(depth, amplitude, carrierF, modulationF));
 
-        Simulator simulator = new Simulator(new Transmitter(modulator), receiver, channel, 0, 17, 0.01);
+        Simulator simulator = new Simulator(new Transmitter(modulator), receiver, channel, 0, 10, 0.001, false);
 
-        new Thread(() -> {
+        Thread t1 = new Thread(() -> {
             while (!receiver.getDemodulator().getDataOut().isClosed()) {
                 try {
                     System.out.println(receiver.getDemodulator().getDataOut().pop());
@@ -92,12 +103,18 @@ public class Main {
                     throw new RuntimeException(e);
                 }
             }
-        }).start();
+        });
+        t1.start();
 
         try {
             simulator.simulate();
+            t1.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        byteListToString(modulator.getSentBytes());
+        byteListToString(receiver.getDemodulator().getReceivedBytes());
+        System.out.println("error rate: " + getBitErrorRate(modulator, receiver));
     }
 }
--- channel-simulation/java-channel-simulation/src/main/java/simulator/Simulator.java
+++ channel-simulation/java-channel-simulation/src/main/java/simulator/Simulator.java
@@ -1,43 +1,57 @@
 package simulator;
 
 import channel.Channel;
+import main.Plotter;
+import org.jfree.data.xy.XYDataItem;
 import receiver.Receiver;
 import transmitter.Transmitter;
+
+import java.util.ArrayList;
 
 public class Simulator {
 
     private final double startTime, endTime, timeStep;
-
     private Transmitter transmitter;
     private Receiver receiver;
     private Channel channel;
+    private final boolean realtime;
 
     public void simulate() throws InterruptedException {
         // convert timeStep (s) into nanoseconds
         long sleepTime = (long) (timeStep * 1_000_000_000);
-        System.out.println(sleepTime);
+        ArrayList<XYDataItem> transmitterOut = new ArrayList<>();
+        ArrayList<XYDataItem> channelOut = new ArrayList<>();
+
         for (double t = startTime; t < endTime; t += timeStep) {
             long deltaT = System.nanoTime();
 
             double f = transmitter.send(t);
+            transmitterOut.add(new XYDataItem(t, f));
             f = channel.output(f);
+            channelOut.add(new XYDataItem(t, f));
             receiver.receive(f, t);
+
+            if (!realtime) continue;
 
             deltaT = sleepTime - (System.nanoTime() - deltaT);
             if (deltaT < 0) System.out.println(deltaT + " " + (System.nanoTime() - deltaT));
             Thread.sleep(deltaT / 1_000_000, (int) (sleepTime - deltaT) % 1_000_000);
         }
 
+        Plotter.plot("Transmitter", "../assets/transmitter.png", "a", "t", new XYDataItem(1600, 900), transmitterOut);
+        Plotter.plot("Channel", "../assets/channel.png", "a", "t", new XYDataItem(1600, 900), channelOut);
+
         receiver.getDemodulator().getDataOut().close();
     }
 
-    public Simulator(Transmitter transmitter, Receiver receiver, Channel channel, double startTime, double endTime, double timeStep) {
+    public Simulator(Transmitter transmitter, Receiver receiver, Channel channel, double startTime, double endTime, double timeStep, boolean realtime) {
         this.transmitter = transmitter;
         this.receiver = receiver;
         this.channel = channel;
         this.startTime = startTime;
         this.endTime = endTime;
         this.timeStep = timeStep;
+        this.realtime = realtime;
     }
 
     public void setTransmitter(Transmitter transmitter) {
