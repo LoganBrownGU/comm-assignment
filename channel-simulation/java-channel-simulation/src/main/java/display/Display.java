175e1bb189a1a2257678e72f5335aec606f5790e
LoganBrownGU <2641407b@student.gla.ac.uk>
2023-12-02 17:57:09 +0000

    scopes working

--- channel-simulation/java-channel-simulation/src/main/java/display/Display.java
+++ channel-simulation/java-channel-simulation/src/main/java/display/Display.java
@@ -7,27 +7,38 @@ import java.awt.event.WindowEvent;
 public class Display extends Frame implements Runnable {
 
     private final Scope transmitterScope, channelScope;
+    private final TextArea dataIn, dataOut;
 
     public void update(double transmitterValue, double channelValue, double timeStep) {
-        transmitterScope.update(transmitterValue, timeStep);
-        channelScope.update(channelValue, timeStep);
+        this.transmitterScope.update(transmitterValue, timeStep);
+        this.channelScope.update(channelValue, timeStep);
     }
 
     private void init() {
         this.setSize(1600, 900);
         Color scopeColour = new Color(0x000088);
 
-        transmitterScope.setSize(this.getWidth() / 4, this.getHeight() / 2);
-        transmitterScope.setLocation(this.getWidth() / 3, 10);
-        transmitterScope.setBackground(scopeColour);
-        transmitterScope.setVisible(true);
-        this.add(transmitterScope);
+        this.transmitterScope.setSize(this.getWidth() / 2, this.getHeight() / 2);
+        this.transmitterScope.setLocation(0, 10);
+        this.transmitterScope.setBackground(scopeColour);
+        this.transmitterScope.setVisible(true);
+        this.add(this.transmitterScope);
 
-        channelScope.setSize(this.getWidth() / 4, this.getHeight() / 2);
-        channelScope.setLocation(2*this.getWidth() / 3, 10);
-        channelScope.setBackground(scopeColour);
-        channelScope.setVisible(true);
-        this.add(channelScope);
+        this.channelScope.setSize(this.getWidth() / 2, this.getHeight() / 2);
+        this.channelScope.setLocation(this.getWidth() / 2, 10);
+        this.channelScope.setBackground(scopeColour);
+        this.channelScope.setVisible(true);
+        this.add(this.channelScope);
+
+        this.dataIn.setSize(100, 20);
+        this.dataIn.setLocation(0, this.getHeight() - this.dataOut.getHeight());
+        this.dataIn.setVisible(true);
+        this.add(this.dataIn);
+
+        this.dataIn.setSize(100, 20);
+        this.dataIn.setLocation(0, this.getHeight() - this.dataOut.getHeight());
+        this.dataIn.setVisible(true);
+        this.add(this.dataIn);
 
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -49,5 +60,7 @@ public class Display extends Frame implements Runnable {
         super(title);
         this.transmitterScope = new Scope();
         this.channelScope = new Scope();
+        this.dataIn = new TextArea();
+        this.dataOut = new TextArea();
     }
 }
--- channel-simulation/java-channel-simulation/src/main/java/display/Scope.java
+++ channel-simulation/java-channel-simulation/src/main/java/display/Scope.java
@@ -6,24 +6,32 @@ import java.util.ArrayList;
 public class Scope extends Canvas {
 
     private Graphics2D graphics = null;
-    private double previous = 0, current = 0;
     private double timeStep;
     private final ArrayList<Double> backlog = new ArrayList<>();
+    private int backlogSize = 1000;
 
     @Override
     public void paint(Graphics g) {
         if (this.graphics == null) this.graphics = (Graphics2D) this.getGraphics();
 
         this.graphics.clearRect(0, 0, this.getWidth(), this.getHeight());
-        graphics.setColor(Color.WHITE);
-        this.graphics.translate(this.getWidth() * timeStep, 0);
-        int yVal = this.getHeight() / 2 + (int) (this.getHeight() * previous / 2);
-        this.graphics.drawLine((int) (this.getWidth() - timeStep), yVal, this.getWidth(), (int) (this.getHeight() * current));
+        this.graphics.setColor(Color.WHITE);
+
+        int prevX = 0, prevY = 0;
+        for (double d : this.backlog) {
+            int y = (int) (d * this.getHeight() / 2) + this.getHeight() / 2;
+            int x = prevX + Math.max(this.getWidth() / this.backlogSize, 1);
+
+            this.graphics.drawLine(prevX, prevY, x, y);
+            prevX = x;
+            prevY = y;
+        }
     }
 
     public void update(double sample, double timeStep) {
         this.backlog.add(sample);
-        if (this.backlog.size() > this.getWidth()) this.backlog.remove(0);
+        this.backlogSize = (int) (1 / timeStep);
+        if (this.backlog.size() > this.backlogSize) this.backlog.remove(0);
         this.timeStep = timeStep;
         this.paint(this.graphics);
     }
