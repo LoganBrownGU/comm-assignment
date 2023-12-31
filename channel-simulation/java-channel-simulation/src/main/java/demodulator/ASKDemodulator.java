07b28fdf63a37f224d006cb5736392cfe3abc9ba
LoganBrownGU <2641407b@student.gla.ac.uk>
2023-12-31 17:37:34 +0000

    commenting

--- channel-simulation/java-channel-simulation/src/main/java/demodulator/ASKDemodulator.java
+++ channel-simulation/java-channel-simulation/src/main/java/demodulator/ASKDemodulator.java
@@ -1,35 +1,44 @@
 package demodulator;
 
-import org.jfree.data.xy.XYDataItem;
-
 public class ASKDemodulator extends Demodulator {
 
     private final float depth, amplitude, modulationPeriod;
-    private float maxAmp = 0;       // max amplitude for this bit frame
-    private int transitions = 0;    // no. bit transitions
-    private byte bitMask = 0x01;
+    private float maxAmp = 0;       // Max. amplitude for a given bit frame.
+    private int transitions = 0;    // No. bit frame transitions.
+    private byte bitMask = 0x01;    // This determines the bit being received for the current byte.
+                                    // For example, if the 3rd least significant bit was being received,
+                                    // then bitMask = 0000 0100
 
     @Override
     public void initialCalculate(float[] amp, float timeStep) {
         this.amp = amp;
     }
 
+
+    // Noise is the actual random gaussian value, not the noise RMS, or SNR.
     @Override
     public void next(float noise) {
         float time = this.timeStep * this.index;
         float f = this.amp[this.index] + noise;
 
         this.maxAmp = Math.max(f, this.maxAmp);
+        // Get the  current bit frame, i.e. the index of the current bit being received.
         int frame = (int) (time / this.modulationPeriod);
 
+        // If the number of bit frame transitions does not equal the current frame, then move to the next frame.
         if (this.transitions != frame) {
-            updateByte(this.bitMask, this.maxAmp > this.amplitude * (1 - this.depth/2));
+            // Take the max amplitude for this frame check if it is greater than halfway between the "high" amplitude and
+            // the "low" amplitude.
+            super.updateByte(this.bitMask, this.maxAmp > this.amplitude * (1 - this.depth/2));
 
             this.maxAmp = 0;
+            // Left shift bitMask by 1 bit.
             this.bitMask <<= 1;
             this.transitions++;
 
-            if (this.bitMask == (byte) 0x00) this.bitMask = 0x01;
+            // Left shifting 1000 0000 will give 0000 0000, so that means a full byte has been received and bitMask needs
+            // to be reset.
+            if (this.bitMask == (byte) 0b0000_0000) this.bitMask = 0b0000_0001;
         }
 
         this.index++;
@@ -43,8 +52,8 @@ public class ASKDemodulator extends Demodulator {
     }
 
     public void reset() {
-        this.index = 0;
-        this.bitMask = 0x01;
+        super.reset();
+        this.bitMask = 0b0000_0001;
         this.transitions = 0;
     }
 }
